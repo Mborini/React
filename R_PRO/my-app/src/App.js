6454a609dff0d9c82f8275490cc9b1c4113d5176
Mborini <mborini98@gmail.com>
2023-05-27 17:00:12 +0300

    f1

---
+++ R_PRO/my-app/src/App.js
@@ -0,0 +1,13 @@
+import BasicExample from './component/card.js';
+import NavBar1 from './component/navbar.js';
+
+const App = () => {
+  return (
+    <div>
+    <NavBar1 />
+    <BasicExample/>
+  
+    </div>);
+};
+
+export default App;
