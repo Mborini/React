f33d10a046f0b7257c384a44f59b378f45d76cb5
Mborini <mborini98@gmail.com>
2023-05-27 17:20:30 +0300

    pop

--- R_PRO/my-app/src/component/card.js
+++ R_PRO/my-app/src/component/card.js
@@ -1,5 +1,5 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import {Button,Card} from 'react-bootstrap/Button';
+
 
 function BasicExample() {
   return (
